b6ff06d56251d9998d585139b58a99ed677d2d55
cfossto <70951673+cfossto@users.noreply.github.com>
2021-04-18 21:17:14 +0200

    Initial commit

---
+++ src/main/java/com/example/theCarbonProject/TheCarbonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.theCarbonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCarbonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCarbonProjectApplication.class, args);
+	}
+
+}
